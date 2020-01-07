3cb56e8808c18a53b5035d1457d93f2333d53962
Andreas Gunnahr <Andreas.Gunnahr@hotmail.com>
2020-01-07 22:37:01 +0100

    added responsiveness to home-, login- and register page

--- middleware/passport-config.js
+++ middleware/passport-config.js
@@ -1,6 +1,7 @@
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
-const db = require('../db/index')
+const db = require('../db/index');
+var validator = require("email-validator");
 
 function initialize(passport, getUserByUsername, getUserById){
     const authenticateUser = async (username, password, done) => {
@@ -20,7 +21,13 @@ function initialize(passport, getUserByUsername, getUserById){
     }
     const authenticateNewUser = async (req,username, password, done) => {
         const user = await getUserByUsername(username);
-        if(user == undefined){
+        if(req.body.name == ""){
+            return done(null, false, {message: 'Please enter your full name'})
+        }
+        else if(!validator.validate(req.body.mail)){
+            return done(null, false, {message: 'Please enter a valid email'})
+        }
+        else if(user == undefined){
             const user = req.body
             user.password = await bcrypt.hash(user.password,10);
             const insertId = await db.insert(`users`, user);
@@ -29,11 +36,15 @@ function initialize(passport, getUserByUsername, getUserById){
         }
         
         return done(null, false, {message: 'Username taken'})
-        // Add if error handling
     }
+
+    // const authenticateNewPost = async (req,username, password, done) => {
+        
+    // }
     
     passport.use('login',new LocalStrategy({usernameField: 'username'}, authenticateUser))
     passport.use('register',new LocalStrategy({usernameField: 'username', passReqToCallback: true}, authenticateNewUser))
+    // passport.use('post',new LocalStrategy({usernameField: 'username', passReqToCallback: true}, authenticateNewPost))
     passport.serializeUser((user,done) => done(null, user.id))
     passport.deserializeUser((id,done) => {
         return done(null, getUserById(id))
