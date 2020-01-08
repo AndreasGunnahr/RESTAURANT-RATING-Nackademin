704c6eca1726ec22b8337508a853835e1a696021
Andreas Gunnahr <Andreas.Gunnahr@hotmail.com>
2020-01-08 16:18:44 +0100

    Added responsiveness to single post page

--- public/javascript/cards.js
+++ public/javascript/cards.js
@@ -12,7 +12,7 @@ if(cards){
         }
         card.addEventListener('click', () => {
             const postID = card.id;
-            location.href = 'https://rate-your-restaurant.herokuapp.com/post/' + postID;
+            location.href = '/post/' + postID;
         })
     });
 }
